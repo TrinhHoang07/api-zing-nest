c66811b91c14eeb8dc69d7a5d6927c29b21205c0
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-08-04 23:05:12 +0700

    update code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Response, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,18 @@ export class AppController {
     @Get()
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get('/test/file')
+    getFile(@Response({ passthrough: true }) res): StreamableFile {
+        res.set({
+            'Content-Type': 'image/webp,image/apng',
+        });
+
+        console.log('J', join(process.cwd(), 'ok_test.jpg'));
+
+        const file = createReadStream(join(process.cwd(), 'ok_test.jpg'));
+
+        return new StreamableFile(file);
     }
 }
